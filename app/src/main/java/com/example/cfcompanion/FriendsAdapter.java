7e75f230116d72da1a51bb7e44d4515d58730da7
Priyansh Mali <priyanshmali18@gmail.com>
2022-08-04 11:56:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cfcompanion/FriendsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.cfcompanion;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+// import com.example.cfcompanion.databinding.ListItemBinding;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendViewHolder> {
+  private static final String TAG = "FriendsAdapter";
+  //  private ListItemBinding listItemBinding;
+  private ArrayList<String> friendHandles;
+  private Context context;
+  private LayoutInflater inflater;
+  ArrayList<String> friend = new ArrayList<String>();
+  int cnt = 0;
+
+  public FriendsAdapter(ArrayList<String> friendHandles, Context context) {
+    this.friendHandles = friendHandles;
+    this.context = context;
+    inflater = LayoutInflater.from(context);
+  }
+
+  @NonNull
+  @Override
+  public FriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View listItemLayoutView = inflater.inflate(R.layout.list_item, parent, false);
+    return new FriendViewHolder(listItemLayoutView, this);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull FriendViewHolder holder, int position) {
+    String currentFriend = friendHandles.get(position);
+    friend.add(currentFriend);
+    holder.friendTextView.setText(Integer.toString(position+1) +". " + currentFriend);
+  }
+
+  @Override
+  public int getItemCount() {
+    return friendHandles.size();
+  }
+
+  public class FriendViewHolder extends RecyclerView.ViewHolder {
+    final TextView friendTextView;
+    final FriendsAdapter friendsAdapter;
+
+    public FriendViewHolder(View layoutView, FriendsAdapter adapter) {
+      super(layoutView);
+      friendTextView = layoutView.findViewById(R.id.friendTextView);
+      friendsAdapter = adapter;
+    }
+  }
+}
